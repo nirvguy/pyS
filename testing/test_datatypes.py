1d8463b109db0d6b27b7d4ceb766686135a49968
Juan Cruz Sosa <nirvguy@gmail.com>
2016-11-07 12:01:19 -0300

    Add tests for S Lists

--- testing/test_datatypes.py
+++ testing/test_datatypes.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import sys
 import unittest
+import itertools
 
 sys.path.append('..')
 
-from pyS.S.datatypes import STuple
+from pyS.S.datatypes import STuple, SList
 
 class TestSTuple(unittest.TestCase):
     RANGE_X = 20
@@ -19,6 +20,23 @@ class TestSTuple(unittest.TestCase):
         for x in range(0, TestSTuple.RANGE_X):
             for y in range(0, TestSTuple.RANGE_Y):
                 self.assertEqual(STuple.encode((x,y)).decode(),(x,y))
+
+
+def all_sequences(k, elems):
+    return (l for i in range(0, k+1) for l in product(elems, repeat=i))
+
+class TestSList(unittest.TestCase):
+    RANGE = 1000
+    RANGE_N = 5
+    RANGE_K = 8
+
+    def test_decode_encode(self):
+        for i in range(1, TestSList.RANGE):
+            self.assertEqual(SList.encode(SList(i).decode()).z, i)
+
+    def test_encode_decode(self):
+        for l in all_sequences(TestSList.RANGE_K, range(1, TestSList.RANGE_N)):
+            self.assertEqual(list(SList.encode(l).decode()),l)
 
 if __name__ == '__main__':
     unittest.main()
